8aa6989b6f13b8a92bd6a7b5bdeec3259b2b143e
estaudt <estaudt@gmail.com>
2015-09-29 21:34:59 -0700

    Initial Commit

---
+++ Background Subtraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Background Subtraction
+//
+//  Created by Elliot Staudt on 9/29/15.
+//  Copyright © 2015 Elliot Staudt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
